ad01df75b5f41ce0f1bb128d8d7036c8b358e2c0
suhut <suhut.wadiyo@gmail.com>
2023-10-13 07:19:25 +0700

    ConsoleAppAuthentication

---
+++ ConsoleAppAuthentication/ConsoleAppAuthentication/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ConsoleAppAuthentication;
+using System.CommandLine;
+
+var root = new RootCommand();
+
+var authCommand = new Command("auth", "Authenticate with local server");
+authCommand.SetHandler(Auth.Handler);
+
+var callCommand = new Command("call", "Perfom authenticated call");
+callCommand.SetHandler(Call.Handler);
+
+root.AddCommand(authCommand);
+root.AddCommand(callCommand);
+
+root.Invoke(args);

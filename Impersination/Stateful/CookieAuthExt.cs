5d827070d381687a0bbd3e03b0c1d7bdad906271
suhut <suhut.wadiyo@gmail.com>
2023-10-14 22:32:30 +0700

    Impersination

---
+++ Impersination/Stateful/CookieAuthExt.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Stateful;
+
+public static class CookieAuthExt
+{
+    public static AuthenticationBuilder AddMyCookie(this AuthenticationBuilder builder, string authenticationScheme, string? displayName, Action<CookieAuthenticationOptions> configureOptions)
+    {
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<CookieAuthenticationOptions>, PostConfigureCookieAuthenticationOptions>());
+        builder.Services.AddOptions<CookieAuthenticationOptions>(authenticationScheme).Validate(o => o.Cookie.Expiration == null, "Cookie.Expiration is ignored, use ExpireTimeSpan instead.");
+        return builder.AddScheme<CookieAuthenticationOptions, CookieAuthenticationHandlerExt>(authenticationScheme, displayName, configureOptions);
+    }
+}

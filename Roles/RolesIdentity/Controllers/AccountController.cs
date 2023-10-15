b998fe24fe4b41e3f9e0c68a263ac09fdbd2429c
suhut <suhut.wadiyo@gmail.com>
2023-10-15 07:51:54 +0700

    Roles

---
+++ Roles/RolesIdentity/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace RolesIdentity.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountController : ControllerBase
+{
+    [HttpGet("/login")]
+    public async Task<IActionResult> Login(SignInManager<IdentityUser> signInManager)
+    {
+        await signInManager.PasswordSignInAsync("test@test.com", "password", false, false);
+
+        return Ok();
+    }
+
+}

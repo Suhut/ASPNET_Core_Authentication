7b51d18f81c57ab49e757877f7ac311dd747064e
suhut <suhut.wadiyo@gmail.com>
2023-10-18 07:44:19 +0700

    CookiesAndTokens

---
+++ CookiesAndTokens/CookiesAndTokens/ApplicationBuilderExtemsions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace CookiesAndTokens;
+
+public static class ApplicationBuilderExtemsions
+{
+    public static async Task<WebApplication> BuildAndSetup(this WebApplicationBuilder builder)
+    {
+        var app = builder.Build();
+        using (var scope = app.Services.CreateScope())
+        {
+            var usrMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var user = new IdentityUser() { UserName = "test@test.com", Email = "test@test.com" };
+            await usrMgr.CreateAsync(user, password: "password");
+            await usrMgr.AddClaimAsync(user, new Claim("role", "janitor"));
+        }
+        return app;
+    }
+}
